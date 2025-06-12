bd7ec92dc4c19734dc8cd750c9016820d9686efc
younjiiichoi <kellychoipanthers@gmail.com>
2025-06-12 14:09:17 +0900

    Update 04_plotlytest.py

--- pages/04_plotlytest.py
+++ pages/04_plotlytest.py
@@ -3,7 +3,8 @@ import pandas as pd
 import plotly.express as px
 
 # 📁 데이터 로딩
-df = pd.read_csv("data.csv", encoding="cp949")
+df = pd.read_csv("people_gender.csv", encoding="cp949")
+df = pd.read_csv("people_sum.csv", encoding="cp949")
 
 # 📍 지역 선택
 region = st.selectbox("📍 지역을 선택하세요", df['행정구역'].unique())
